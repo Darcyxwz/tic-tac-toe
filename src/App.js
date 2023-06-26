e05a4537f5ad0933737632d9cb2217fc51149bd2
Darcyxwz <1003669366@qq.com>
2023-06-26 14:47:00 +0800

    1

--- src/App.js
+++ src/App.js
@@ -10,15 +10,17 @@ export default function Board() {
   const [values, setValues] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    setOrder(order + 1);
-    const nextValues = values.slice();
-    if (order % 2 == 0) {
-      nextValues[i] = 'O';
-      setValues(nextValues);
-    }
-    else {
-      nextValues[i] = 'X';
-      setValues(nextValues);
+    if (values[i] == null) {
+      setOrder(order + 1);
+      const nextValues = values.slice();
+      if (order % 2 == 0) {
+        nextValues[i] = 'O';
+        setValues(nextValues);
+      }
+      else {
+        nextValues[i] = 'X';
+        setValues(nextValues);
+      }
     }
   }
 
