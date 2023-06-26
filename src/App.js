d87b5077a53db98f9eb1ba1d3a4beee5d8395dba
Darcyxwz <1003669366@qq.com>
2023-06-26 11:19:30 +0800

    1

--- src/App.js
+++ src/App.js
@@ -7,33 +7,40 @@ function Square({ value, onClick }) {
 
 export default function Board() {
   const [order, setOrder] = useState(0);
-  const [value, setValue] = useState(null);
-  function handleClick() {
+  const [values, setValues] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
     setOrder(order + 1);
+    const nextValues = values.slice();
     if (order % 2 == 0) {
-      setValue('O');
+      nextValues[i] = 'O';
+      setValues(nextValues);
     }
     else {
-      setValue('X');
+      nextValues[i] = 'X';
+      setValues(nextValues);
     }
   }
+
   return (
     <div>
       <div className = "board-row">
-        <Square onClick = {handleClick} value = {value} />
-        <Square onClick = {handleClick} value = {value} />
-        <Square onClick = {handleClick} value = {value} />
+        <Square onClick = {() => handleClick(0)} value = {values[0]} />
+        <Square onClick = {() => handleClick(1)} value = {values[1]} />
+        <Square onClick = {() => handleClick(2)} value = {values[2]} />
       </div>
       <div className = "board-row">
-       <Square onClick = {handleClick} value = {value} />
-       <Square onClick = {handleClick} value = {value} />
-       <Square onClick = {handleClick} value = {value} />
+       <Square onClick = {() => handleClick(3)} value = {values[3]} />
+       <Square onClick = {() => handleClick(4)} value = {values[4]} />
+       <Square onClick = {() => handleClick(5)} value = {values[5]} />
       </div>
       <div className = "board-row">
-       <Square onClick = {handleClick} value = {value} />
-       <Square onClick = {handleClick} value = {value} />
-       <Square onClick = {handleClick} value = {value} />
+       <Square onClick = {() => handleClick(6)} value = {values[6]} />
+       <Square onClick = {() => handleClick(7)} value = {values[7]} />
+       <Square onClick = {() => handleClick(8)} value = {values[8]} />
       </div>
     </div>
   );
 }
+
+
